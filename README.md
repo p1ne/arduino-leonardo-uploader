13090e804900bc38d796174b7effc6ab58d18186
p1ne <nnalutin@gmail.com>
2017-06-29 14:05:01 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # arduino-leonardo-uploader
 Simple wrapper for avrdude to allow firmware upload to Arduino Leonardo from command line
+
+That's very common problem referenced on Arduino forums and Stackexchange - when you try to upload 
+firmware to Arduino Leonardo and clones with avrdude, you get programmer timeout error message.
+
+Reason for it is very simple - Arduino Leonardo should be reset to provide another serial port for flashing
+
+This repository contains simple Windows-based bat file that identifies your Arduino Leonardo COM port through WMIC, performs COM port reset, then identifies bootloader COM port and invokes avrdude to upload firmware from firmware.hex file
+
+firmware.hex file is empty here, please put yours. 
+
+avrdude binary and config is taken from Arduino IDE 1.8.3
+
+OS X and Linux version coming soon, idea is pretty much the same - to use stty to control serial port.
---
+++ windows/upload.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal
+
+for /f "tokens=1* delims==" %%I in ('wmic path win32_pnpentity get caption  /format:list ^| find "SparkFun Pro Micro"') do (
+    call :resetCOM "%%~J"
+)
+
+:continue
+
+:: wmic /format:list strips trailing spaces (at least for path win32_pnpentity)
+for /f "tokens=1* delims==" %%I in ('wmic path win32_pnpentity get caption  /format:list ^| find "Arduino Leonardo bootloader"') do (
+    call :setCOM "%%~J"
+)
+
+:: end main batch
+goto :EOF
+
+:resetCOM <WMIC_output_line>
+:: sets _COM#=line
+setlocal
+set "str=%~1"
+set "num=%str:*(COM=%"
+set "num=%num:)=%"
+set port=COM%num%
+echo %port%
+mode %port%: BAUD=1200 parity=N data=8 stop=1
+goto :continue
+
+:setCOM <WMIC_output_line>
+:: sets _COM#=line
+setlocal
+set "str=%~1"
+set "num=%str:*(COM=%"
+set "num=%num:)=%"
+set port=COM%num%
+echo %port%
+goto :flash
+
+:flash
+avrdude -v -C./avrdude.conf -patmega32u4 -cavr109 -P%port% -b57600 -D -V -Uflash:w:./firmware.hex:i
